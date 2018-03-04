1542338403c4370d0e4c60fa37f56dece2aaa17b
Pavel Podoprigora <pavelqwerty1998@gmail.com>
2018-03-04 22:27:24 +0300

    js

---
+++ Site/script.js
@@ -0,0 +1,153 @@
+var photoPost = [
+    {
+        id:"1",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:00:00'),
+        author:'Pavel Podoprigora',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"2",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:23:00'),
+        author:'Nikita Grishin',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"3",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:22:00'),
+        author:'Andron Butirca',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"4",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:21:00'),
+        author:'Prishep Mac',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"5",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:20:00'),
+        author:'Dimas Kotus',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"6",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:19:00'),
+        author:'Valai San',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"8",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:18:00'),
+        author:'Glush Fil',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"9",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:17:00'),
+        author:'Kusmuk San',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"10",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:16:00'),
+        author:'Naum Dan',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"11",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:15:00'),
+        author:'Gusak San',
+        photoLink:''
+    },
+    {
+        id:"12",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:14:00'),
+        author:'Nikifor Nik',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"13",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:13:00'),
+        author:'Dimas Gabr',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"14",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:12:00'),
+        author:'Djuk Kat',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"15",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:11:00'),
+        author:'Ketok Jen',
+        photoLink:''
+    },
+    {
+        id:"16",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:10:00'),
+        author:'Veron belka',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"17",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:9:00'),
+        author:'San Gush',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"18",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:8:00'),
+        author:'Gus San',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"19",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:7:00'),
+        author:'Sis Ila',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id:"20",
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Data('2018-02-23T32:6:00'),
+        author:'Lis Jak',
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+]
+
+(function () {
+    function getPhotopost(id) {
+        if(Number(id) > 20 || Number(id) < 1 || typeof id !== "string"){
+            return 0;
+        }
+        for(var i = 0;i<photoPost.length();i++){
+            if(photoPost[i].id == id){
+                return photoPost;
+            }
+        }
+    }
+})
+
+function () {
+    function
+}
+
