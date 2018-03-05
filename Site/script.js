7ae92592a92bb67d647fdcc32efe81735203db57
Pavel Podoprigora <pavelqwerty1998@gmail.com>
2018-03-05 20:17:09 +0300

    likes + hashtags

--- Site/script.js
+++ Site/script.js
@@ -4,133 +4,171 @@ var photoPost = [
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:00:00'),
         author:'Pavel Podoprigora',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#ram','#rider'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"2",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Data('2018-02-23T32:23:00'),
+        createdAt: new Data('2018-02-22T32:23:00'),
         author:'Nikita Grishin',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#smile','#long'],
+        likes:['Nikita Grishin','Andron Butirca','Pavel Podoprigora']
     },
     {
         id:"3",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Data('2018-02-23T32:22:00'),
+        createdAt: new Data('2018-02-21T32:22:00'),
         author:'Andron Butirca',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#linda','#lolo'],
+        likes:['Nikita Grishin','Prishep Mac']
     },
     {
         id:"4",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:21:00'),
         author:'Prishep Mac',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#luntik','lulu'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"5",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:20:00'),
         author:'Dimas Kotus',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#ramzi','#ramzes'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"6",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:19:00'),
         author:'Valai San',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#ramires','#raddj'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"8",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:18:00'),
         author:'Glush Fil',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#Minsk','#Belarus'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"9",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:17:00'),
         author:'Kusmuk San',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#01','#2018'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"10",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:16:00'),
         author:'Naum Dan',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#New_year','#2019'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"11",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:15:00'),
         author:'Gusak San',
-        photoLink:''
+        photoLink:'',
+        hashtag:['#very','#old'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"12",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:14:00'),
         author:'Nikifor Nik',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#shersh','#problems'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"13",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:13:00'),
         author:'Dimas Gabr',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#gta5','#top_in_theWorld'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"14",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:12:00'),
         author:'Djuk Kat',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#true'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"15",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:11:00'),
         author:'Ketok Jen',
-        photoLink:''
+        photoLink:'',
+        hashtag:['#false'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"16",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:10:00'),
         author:'Veron belka',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#backend'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"17",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:9:00'),
         author:'San Gush',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#frontend'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"18",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:8:00'),
         author:'Gus San',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#oracle','#fpm'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"19",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:7:00'),
         author:'Sis Ila',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#Noon'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
     {
         id:"20",
         description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
         createdAt: new Data('2018-02-23T32:6:00'),
         author:'Lis Jak',
-        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        photoLink:'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtag:['#Son','#moon'],
+        likes:['Nikita Grishin','Andron Butirca']
     },
 ]
 
